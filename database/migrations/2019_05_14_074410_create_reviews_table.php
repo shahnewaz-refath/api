26b2022ad1fa0ceef288ba061489a14d1e2e0630
refath <refathctg@gmail.com>
2019-05-14 16:01:22 +0600

    migration created

--- database/migrations/2019_05_14_074410_create_reviews_table.php
+++ database/migrations/2019_05_14_074410_create_reviews_table.php
@@ -15,6 +15,11 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id')->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer',191);
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
